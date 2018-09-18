82bc2d92545df57f1207e765d464a9a957d4583b
Olivia Kasten <odkasten@gmail.com>
2018-09-18 01:21:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,12 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index {|dwarf, i| puts "#{i+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |call|
+    call = call.first.upcase
+    call << "!"
+  end 
 end
 
 def long_planeteer_calls# code an argument here
