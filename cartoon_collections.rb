ff2a9d8a4a91d96f9241f934bcf744db87def123
Olivia Kasten <odkasten@gmail.com>
2018-09-18 01:29:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  array.select do |word|
+    array.include?(cheese_types)
+  end 
 end
